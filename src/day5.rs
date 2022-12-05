8783f4169f96f1bb3c26cd994e79cff09066329a
Mathieu Nocenti <mathieu.nocenti@gmail.com>
2022-12-05 22:40:25 +0100

    day 5 : use vec instead of VecDeque

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use std::{collections::VecDeque, fmt::Display, str::FromStr};
+use std::{fmt::Display, str::FromStr};
 
 use itertools::Itertools;
 
@@ -37,7 +37,7 @@ impl Display for Instruction {
     }
 }
 
-type Stack = VecDeque<char>; //< crates, top to bottom
+type Stack = Vec<char>; //< crates, top to bottom
 
 #[derive(Debug, Default)]
 struct Stacks {
@@ -45,15 +45,16 @@ struct Stacks {
 }
 
 impl Stacks {
-    fn parse(lines: impl Iterator<Item = String>) -> utils::MyResult<Stacks> {
-        let mut lines = lines.peekable();
+
+    /// parse the first part of the input as a Stacks object
+    fn parse(lines: Vec<String>) -> utils::MyResult<Stacks> {
         let mut stacks = Stacks::default();
-        stacks.stacks.resize(lines.peek().ok_or_else(||"Bad stacks input")?.len() / 4 + 1, Stack::new());
-        for l in lines {
+        stacks.stacks.resize(lines[0].len()/ 4 + 1, Stack::new());
+        for l in lines.into_iter().rev() {
             for stack in 0..stacks.stacks.len() {
                 let c = l.chars().nth(4*stack+1).ok_or_else(||"Bad stacks input")?;
                 if !c.is_numeric() && !c.is_whitespace() {
-                    stacks.stacks[stack].push_back(c);
+                    stacks.stacks[stack].push(c);
                 }
             }
         }
@@ -62,25 +63,23 @@ impl Stacks {
 
     /// Get the top of the stacks as a String
     fn top(&self) -> String {
-        (&self.stacks).into_iter().filter_map(Stack::front).collect()
+        (&self.stacks).into_iter().filter_map(|s| s.last()).collect()
     }
 
     /// Apply a given instruction to the stacks using CrateMover 9000
-    fn apply_9000(&mut self, instruction: Instruction) -> utils::MyResult<()> {
-        for _ in 0..instruction.count {
-            let a = self.stacks[instruction.source].pop_front().ok_or(":(")?;
-            self.stacks[instruction.dest].push_front(a);
+    fn apply_9000(&mut self, inst: Instruction) -> utils::MyResult<()> {
+        for _ in 0..inst.count {
+            let a = self.stacks[inst.source].pop().ok_or(":(")?;
+            self.stacks[inst.dest].push(a);
         }
         Ok(())
     }
 
     /// Apply a given instruction to the stacks using CrateMover 9001
-    fn apply_9001(&mut self, instruction: Instruction) -> utils::MyResult<()> {
-        let mut new_dest: VecDeque<char> = self.stacks[instruction.source]
-            .drain(0..instruction.count)
-            .collect();
-        new_dest.append(&mut self.stacks[instruction.dest]);
-        self.stacks[instruction.dest] = new_dest;
+    fn apply_9001(&mut self, inst: Instruction) -> utils::MyResult<()> {
+        let src = &mut self.stacks[inst.source];
+        let top_crates = src.split_off(src.len()-inst.count);
+        self.stacks[inst.dest].extend(top_crates);
         Ok(())
     }
 }
@@ -98,17 +97,11 @@ fn crate_mover(
     apply_instructions: impl Fn(&mut Stacks, Instruction) -> utils::MyResult<()>
 ) -> utils::MyResult<()> {
     let mut lines = utils::read_lines("inputs/input5.txt")?;
-
-    let mut stacks = Stacks::parse((&mut lines).take_while(|s| !s.is_empty()))?;
-
-    let instructions = lines.filter_map(|l| Instruction::from_str(&l).ok());
-
-    for instruction in instructions {
-        apply_instructions(&mut stacks, instruction)?;
-    }
-
+    let mut stacks = Stacks::parse((&mut lines).take_while(|s| !s.is_empty()).collect())?;
+    lines
+        .filter_map(|l| Instruction::from_str(&l).ok())
+        .try_for_each(|instruction| { apply_instructions(&mut stacks, instruction)})?;
     println!("{}\n", stacks.top());
-
     Ok(())
 }
 
